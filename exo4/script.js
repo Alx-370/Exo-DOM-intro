317d87ab0a1b02e64bb3f760d0484095067542b3
Alex <alexismoreau37320@gmail.com>
2025-05-19 15:59:34 +0200

    exo4

---
+++ exo4/script.js
@@ -0,0 +1,10 @@
+
+
+const card2 = document.querySelector(".second-card");
+const newCatImage = document.createElement('img');
+
+
+newCatImage.src = "https://s2.dmcdn.net/v/8yz4x1LWQAXG0L7_X/x480";
+newCatImage.alt = "chat trop mignon";
+
+card2.appendChild(newCatImage);
